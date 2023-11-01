06b51d54ac62d9fa106c0519e5298da65ab7f1fb
elghali wali alami <ghaliwali@gmail.com>
2023-11-01 10:22:07 +0100

    Open Graphs are done

---
+++ Little lemon/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Your Website</p>
+    </footer>
+  )
+}
+
+export default footer
---
+++ Little lemon/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+    <section>
+      <h1>LITTLE LEMON</h1>
+      <h2>Chicago</h2>
+      <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+      <button>Reserve a Table</button>
+    </section>
+  </main>
+  )
+}
+
+export default Main
+
